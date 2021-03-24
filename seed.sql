47416f046d7f20265091f16ef1ca44bbd2ae4c82
amcourson <amcourson09@sbcglobal.net>
2021-03-24 12:57:27 -0500

    creating table

---
+++ seed.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS seed_db;
+CREATE DATABASE seed_db;
+
+USE DATABASE seed_db;
+
+CREATE TABLE seed_db;
+    department VARCHAR(30) NOT NULL;
+    employee_role VARCHAR(30) NOT NULL;
+    employee_name VARCHAR(30) NOT NULL;
+
+
