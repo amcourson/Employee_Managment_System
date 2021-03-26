b0a695179ccd84e80f6fa6f39729ad8e3cba71bb
amcourson <amcourson09@sbcglobal.net>
2021-03-25 20:55:45 -0500

    adding to server.js

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-const fs = require(fs);
-const 
+const fs = require('fs');
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+
+
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+  
+    port: 3306,
+  
+    user: 'root',
+  
+    password: 'Sheba1492!',
+    database: 'seed_db',
+  });
+  
