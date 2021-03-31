72a0a1f5e8ebc26855097329b50199baadd1da45
amcourson <amcourson09@sbcglobal.net>
2021-03-30 19:40:43 -0500

    adding functionality

--- index.js
+++ index.js
@@ -4,28 +4,64 @@ const inquirer = require('inquirer');
 
 
 
-const songSearch = () => {
+const connection = mysql.createConnection({
+    host: 'localhost',
+  
+    port: 3306,
+  
+    user: 'root',
+  
+    password: 'Sheba1492!',
+    database: 'seed_db',
+  });
+  
+
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log(`connected as id ${connection.threadId}`);
+  startProgram ();
+  
+});
+
+
+
+
+const startProgram = () => {
   inquirer
     .prompt({
-      name: 'song',
-      type: 'input',
-      message: 'What song would you like to look for?',
+      name: 'start',
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: ["Add department","Add employee", "Update employee", "Add role","view department", "view role", "view employee"],
+      
     })
     .then((answer) => {
-      console.log(answer.song);
-      connection.query(
-        'SELECT * FROM top5000 WHERE ?',
-        { song: answer.song },
-        (err, res) => {
-          if (res[0]) {
-            console.log(
-              `Position: ${res[0].position} || Song: ${res[0].song} || Artist: ${res[0].artist} || Year: ${res[0].year}`
-            );
-          } else {
-            console.error(`No results for ${answer.song}`);
-          }
-          runSearch();
-        }
-      );
+      console.log(answer.start);
+      if(answer.start === "view employee"){
+        connection.query(
+            'SELECT * FROM employee',
+           (err,res)=> {
+             for (let i = 0; i < res.length; i++){
+              console.log(`${res[i].first_name} ${res[i].last_name}`);
+             }
+           }
+            // { song: answer.song },
+            // (err, res) => {
+            //   if (res[0]) {
+            //     console.log(
+            //       `Position: ${res[0].position} || Song: ${res[0].song} || Artist: ${res[0].artist} || Year: ${res[0].year}`
+            //     );
+            //   } else {
+            //     console.error(`No results for ${answer.song}`);
+            //   }
+            //   runSearch();
+            // }
+          );
+      } if(answer.start === "view role"){
+        
+      }
+      
     });
 };
+
--- seed.sql
+++ seed.sql
@@ -7,17 +7,21 @@ CREATE TABLE employee (
     first_name VARCHAR(30)NOT NULL,
     last_name VARCHAR(30)NOT NULL,
     role_id INT,
-    manager_id INT,
+    manager_id INT
 );
 
 CREATE TABLE employee_role(
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(15) NULL,
-    department_id INT(15) NOT NULL,
+    department_id INT(15) NOT NULL
 );
 
 CREATE TABLE department(
-    person_name VARCHAR(30) NOT NULL,
-);
+    person_name VARCHAR(30) NOT NULL
 
+);
+INSERT INTO employee (
+    first_name, last_name, role_id, manager_id
+)
+VALUES ("John","Disney", 1234, 5678)
 
